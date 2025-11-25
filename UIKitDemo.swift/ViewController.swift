5e81a19b02c060b6dad3d531eaf59fbc35b372e6
Shohei <yukawa.shouhei@gmail.com>
2025-11-25 21:36:15 +0900

    //で各実装の役割説明を追加。

--- UIKitDemo.swift/ViewController.swift
+++ UIKitDemo.swift/ViewController.swift
@@ -23,9 +23,13 @@ class ViewController: UIViewController {
         countLabel.textAlignment = .center
         
         //位置とサイズをピクセル単位で命令。 座標(0, 200)に、幅は画面いっぱい、高さは50で配置。
+        //この実装では0~の数字
         countLabel.frame = CGRect(x: 0, y: 200, width: view.frame.width, height: 50)
+        
+        //countLabelを親画面に追加するように命令
         view.addSubview(countLabel)
         
+        //この実装では増やすボタン
         countButton.setTitle("増やす", for: .normal)
         countButton.titleLabel?.font = .systemFont(ofSize: 24)
 
@@ -40,8 +44,10 @@ class ViewController: UIViewController {
         
         count += 1
         
+        //データの変更は、画面に自動反映されないので下記で新しい数字に置き換えるよう命令。
         countLabel.text = "\(count)"
         
+        // 色変えも手動。もし10を超えていたら、色を赤に塗り替えろ！と命令。
         if count > 10 {
             countLabel.textColor = .red
         }
